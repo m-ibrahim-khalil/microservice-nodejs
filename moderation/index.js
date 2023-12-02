7bf1ed49848741cc7af33dbb8316c250b9029441
mdIbrahim-khalil <bsse1009@iit.du.ac.bd>
2023-12-03 02:30:29 +0600

    added different microservices

---
+++ moderation/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const handleEvent = async (type, data) => {
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+};
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  await handleEvent(type, data);
+  res.send({});
+});
+
+app.listen(4003, async () => {
+  console.log("Listening on 4003");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      await handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
